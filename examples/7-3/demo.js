307c68212271db178dcacd2f1049022a7a42e7b1
Mark212160 <mark212160@gmail.com>
2021-10-29 10:29:35 +0800

    Update demo.js

---
+++ examples/7-3/demo.js
@@ -0,0 +1,13 @@
+// Single-line comment
+
+/* Multi-line comment:
+   When you have lots to say, you need this.
+*/
+
+// A "statement" ends with a semi-colon
+console.log('This is an embedded script.');
+
+// A "code block" is defined with a pair of curly brackets {}
+{
+	console.log('This message is from a code block.');
+}
